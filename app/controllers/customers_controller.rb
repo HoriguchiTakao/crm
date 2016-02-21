0d98c901549d15d9897ef52accbd1d55a43044cc
takao.horiguchi <horiguchi.t@opst.co.jp>
2016-02-21 10:54:52 +0900

    initial commit

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,61 @@
+class CustomersController < ApplicationController
+  # それぞれのactionの前に呼ばれる
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  before_action :set_company, only: [:edit, :new]
+
+  def index
+    # kaminariを入れたので.pageが使える
+    @customers = Customer.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+        # true
+        redirect_to @customer
+
+      else
+        # false
+        render :new
+      end
+  end
+
+  def update
+    if @customer.update(customer_params)
+      #true
+    redirect_to @customer
+  else
+    #false
+    render :edit
+  end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to customers_url
+  end
+  # -----------------------------ここからprivate----------------------------------------
+  private
+  def customer_params
+    params.require(:customer).permit(:family_name, :given_name, :email, :company_id)
+  end
+
+  def set_customer
+  @customer = Customer.find(params[:id])
+  end
+
+  def set_company
+  @companies = Company.all
+  end
+
+end
---
+++ app/models/customer.rb
@@ -0,0 +1,16 @@
+class Customer < ActiveRecord::Base
+
+  belongs_to :company
+
+  #validation :対象のカラム, バリデーションの種類
+  validates :family_name, presence: true, length: { maximum: 20 }
+  validates :given_name, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :company_id, presence: true
+
+  def full_name
+    full_name = family_name + given_name + "様"
+    return full_name
+  end
+end
