04d21da232fbb6a57e61942fe6a4be29ae221cf8
takao.horiguchi <horiguchi.t@opst.co.jp>
2016-02-21 14:48:54 +0900

    add family_name and given_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    if family_name?
+      full_name = family_name + given_name
+    else
+      family_name = 'family_name'
+      given_name = 'given_name'
+      full_name = family_name + given_name
+    end
+    return full_name
+  end
+
 end
